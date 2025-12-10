f1e11e00cfed9c5e3801a788f8661bb32e711d43
Joshua Gato <joshuagato37@gmail.com>
2025-12-10 19:10:53 +0000

    Updated formatting for l eft side bar

--- app/components/Sidebar/Left/index.vue
+++ app/components/Sidebar/Left/index.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="flex flex-col h-screen">
-
-
-        <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="defaultTransition">
+        <div
+            class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+            :class="defaultTransition"
+        >
             <nuxt-link to="/">
                 <div class="w-8 h-8">
                     <LogoTwitter />
@@ -11,88 +12,65 @@
         </div>
 
         <div class="mt-2 space-y-3">
-
             <SidebarLeftTab active>
                 <template v-slot:icon>
                     <HomeIcon />
                 </template>
-                <template v-slot:name>
-                    Home
-                </template>
+                <template v-slot:name> Home </template>
             </SidebarLeftTab>
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <HashtagIcon />
                 </template>
-                <template v-slot:name>
-                    Explore
-                </template>
+                <template v-slot:name> Explore </template>
             </SidebarLeftTab>
-
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <BellIcon />
                 </template>
-                <template v-slot:name>
-                    Notifications
-                </template>
+                <template v-slot:name> Notifications </template>
             </SidebarLeftTab>
-
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <InboxIcon />
                 </template>
-                <template v-slot:name>
-                    Messages
-                </template>
+                <template v-slot:name> Messages </template>
             </SidebarLeftTab>
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <BookmarkIcon />
                 </template>
-                <template v-slot:name>
-                    Bookmarks
-                </template>
+                <template v-slot:name> Bookmarks </template>
             </SidebarLeftTab>
-
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <DocumentTextIcon />
                 </template>
-                <template v-slot:name>
-                    Lists
-                </template>
+                <template v-slot:name> Lists </template>
             </SidebarLeftTab>
-
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <UserIcon />
                 </template>
-                <template v-slot:name>
-                    Profile
-                </template>
+                <template v-slot:name> Profile </template>
             </SidebarLeftTab>
 
             <SidebarLeftTab>
                 <template v-slot:icon>
                     <DotsCircleHorizontalIcon />
                 </template>
-                <template v-slot:name>
-                    More
-                </template>
+                <template v-slot:name> More </template>
             </SidebarLeftTab>
 
             <div class="hidden xl:block">
                 <UIButton liquid size="lg" @on-click="emits('onTweet')">
-                    <span class="font-bold">
-                        Tweet
-                    </span>
+                    <span class="font-bold"> Tweet </span>
                 </UIButton>
             </div>
 
@@ -103,14 +81,18 @@
                     </div>
                 </UIButton>
             </div>
-
         </div>
 
-        <div class="flex flex-row items-center justify-center px-2 py-2 mx-auto mt-auto mb-5 rounded-full cursor-pointer w-14 xl:w-full hover:bg-gray-100 dark:hover:bg-dim-800"
-            :class="defaultTransition" @click="emits('onLogout')">
-
+        <div
+            class="flex flex-row items-center justify-center px-2 py-2 mx-auto mt-auto mb-5 rounded-full cursor-pointer w-14 xl:w-full hover:bg-gray-100 dark:hover:bg-dim-800"
+            :class="defaultTransition"
+            @click="emits('onLogout')"
+        >
             <div class="flex flex-row">
-                <img :src="props.user.profileImage" class="w-10 h-10 rounded-full">
+                <img
+                    :src="props.user.profileImage"
+                    class="w-10 h-10 rounded-full"
+                />
                 <div class="flex-col hidden ml-2 xl:block">
                     <h1 class="text-sm font-bold text-gray-800 dark:text-white">
                         {{ user.name }}
@@ -119,7 +101,6 @@
                         {{ user.handle }}
                     </p>
                 </div>
-
             </div>
 
             <!-- ICON -->
@@ -128,25 +109,30 @@
                     <ChevronDownIcon />
                 </div>
             </div>
-
-
-
         </div>
-
     </div>
 </template>
 <script setup>
-import { HomeIcon } from "@heroicons/vue/solid"
-import { HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, DocumentTextIcon, UserIcon, DotsCircleHorizontalIcon, PencilIcon, ChevronDownIcon } from "@heroicons/vue/outline"
+import { HomeIcon } from "@heroicons/vue/solid";
+import {
+    HashtagIcon,
+    BellIcon,
+    InboxIcon,
+    BookmarkIcon,
+    DocumentTextIcon,
+    UserIcon,
+    DotsCircleHorizontalIcon,
+    PencilIcon,
+    ChevronDownIcon,
+} from "@heroicons/vue/outline";
 
-const { defaultTransition } = useTailwindConfig()
-const emits = defineEmits(['onTweet', 'onLogout'])
+const { defaultTransition } = useTailwindConfig();
+const emits = defineEmits(["onTweet", "onLogout"]);
 
 const props = defineProps({
     user: {
         type: Object,
-        required: true
-    }
-})
-
+        required: true,
+    },
+});
 </script>
