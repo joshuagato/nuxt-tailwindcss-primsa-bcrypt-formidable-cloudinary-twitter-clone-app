f09919c8e57040e37f58d5cfe080ebddfff297a9
Joshua Gato <joshuagato37@gmail.com>
2025-12-10 17:20:25 +0000

    Added tailwind config to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindConfig from "./tailwind.config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     modules: ["@nuxtjs/tailwindcss"],
+
+    tailwindcss: {
+        config: tailwindConfig,
+    },
 });
