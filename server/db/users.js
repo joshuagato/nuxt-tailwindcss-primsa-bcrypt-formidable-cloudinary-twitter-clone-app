a47a696609422308b2f744cdfb8644a815eb7dab
Joshua Gato <joshuagato37@gmail.com>
2025-12-11 14:11:24 +0000

    Added get user by email and reformatted file

--- server/db/users.js
+++ server/db/users.js
@@ -1,30 +1,37 @@
 import { prisma } from ".";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 export const createUser = (userData) => {
     const finalUserData = {
         ...userData,
-        password: bcrypt.hashSync(userData.password, 10)
-    }
+        password: bcrypt.hashSync(userData.password, 10),
+    };
 
     return prisma.user.create({
-        data: finalUserData
-    })
-}
-
+        data: finalUserData,
+    });
+};
 
 export const getUserByUsername = (username) => {
     return prisma.user.findUnique({
         where: {
-            username
-        }
-    })
-}
+            username,
+        },
+    });
+};
 
 export const getUserById = (userId) => {
     return prisma.user.findUnique({
         where: {
-            id: userId
-        }
-    })
-}
+            id: userId,
+        },
+    });
+};
+
+export const getUserByEmail = (email) => {
+    return prisma.user.findFirst({
+        where: {
+            email,
+        },
+    });
+};
