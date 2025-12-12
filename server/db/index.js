4aeb4fb2e806d7ea057e6a8e4ad85b8c0f467c4b
Joshua Gato <joshuagato37@gmail.com>
2025-12-12 11:37:54 +0000

    Updated prisma instantiation file

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
-import Prisma from "@prisma/client"
+// import Prisma from "@prisma/client"
+import { PrismaClient } from "../../app/generated/prisma/client";
 
-const { PrismaClient } = Prisma
+// const { PrismaClient } = Prisma;
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export { prisma }
+export { prisma };
