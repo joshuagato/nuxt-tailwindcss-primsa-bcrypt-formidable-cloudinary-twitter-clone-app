92d744f57110728fb5e485e629bfed706069cc2f
Joshua Gato <joshuagato37@gmail.com>
2025-12-12 14:49:14 +0000

    File reformatted

--- app/components/Tweet/Form/index.vue
+++ app/components/Tweet/Form/index.vue
@@ -1,55 +1,60 @@
 <template>
     <div>
-
         <div v-if="loading" class="flex items-center justify-center py-6">
             <UISpinner />
         </div>
         <div v-else>
-            <TweetItem :tweet="props.replyTo" v-if="props.replyTo && props.showReply" hideActions />
-            <TweetFormInput :placeholder="props.placeholder" :user="props.user" @onSubmit="handleFormSubmit" />
+            <TweetItem
+                :tweet="props.replyTo"
+                v-if="props.replyTo && props.showReply"
+                hideActions
+            />
+            <TweetFormInput
+                :placeholder="props.placeholder"
+                :user="props.user"
+                @onSubmit="handleFormSubmit"
+            />
         </div>
-
     </div>
 </template>
 <script setup>
-const emits = defineEmits(['onSuccess'])
-const loading = ref(false)
-const { postTweet } = useTweets()
+const emits = defineEmits(["onSuccess"]);
+const loading = ref(false);
+const { postTweet } = useTweets();
 
 const props = defineProps({
     user: {
         type: Object,
-        required: true
+        required: true,
     },
     placeholder: {
         type: String,
-        default: "What's happening ?"
+        default: "What's happening ?",
     },
     replyTo: {
         type: Object,
-        default: null
+        default: null,
     },
     showReply: {
         type: Boolean,
-        default: false
-    }
-})
+        default: false,
+    },
+});
 
 async function handleFormSubmit(data) {
-    loading.value = true
+    loading.value = true;
     try {
         const response = await postTweet({
             text: data.text,
             mediaFiles: data.mediaFiles,
-            replyTo: props.replyTo?.id
-        })
+            replyTo: props.replyTo?.id,
+        });
 
-        emits('onSuccess', response.tweet)
+        emits("onSuccess", response.tweet);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     } finally {
-        loading.value = false
+        loading.value = false;
     }
 }
-
 </script>
